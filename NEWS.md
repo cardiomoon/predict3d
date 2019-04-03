3a40026e01baf693c7cecc2bd7b1b2486272b149
Keon-Woong Moon <cardiomoon@gmail.com>
2019-04-03 16:23:39 +0900

    0.1.3
    0.1.3

--- NEWS.md
+++ NEWS.md
@@ -1,10 +1,16 @@
-predict3d version 0.1.0
+predict3 version 0.1.3
 =======================
-(2019-Mar-6)
+(2019-Apr-3)
 
-# New function ggPredict() and predict3d() added
+# New function gg_color_hue() and theme_bw2() added
 
-# New function fit2newdata() added
+predict3d version 0.1.2
+=======================
+(2019-Mar-11)
+
+# Bug-fixed
+
+# New function restoreData2(), restoreData3()  added
 
 
 predict3d version 0.1.1
@@ -16,10 +22,13 @@ predict3d version 0.1.1
 # New function add_lines() added
 
 
-predict3d version 0.1.2
+
+predict3d version 0.1.0
 =======================
-(2019-Mar-11)
+(2019-Mar-6)
 
-# Bug-fixed
+# New function ggPredict() and predict3d() added
 
-# New function restoreData2(), restoreData3()  added
+# New function fit2newdata() added
+
+
--- R/add_lines.R
+++ R/add_lines.R
@@ -28,12 +28,21 @@ calEquation=function(fit,mode=1,pred=NULL,modx=NULL,modx.values=NULL,label=NULL,
         modx
         if(is.null(label)) label=modx
         intercept=modx.values*fit$coef[modx]+fit$coef[1]
-        slope=modx.values*fit$coef[4]+fit$coef[pred]
+        slope=modx.values*fit$coef[paste0(pred,":",modx)]+fit$coef[pred]
         labels=paste0(label,"=",round(modx.values,digits))
         df=data.frame(intercept,slope,label=labels)
         df
 }
 
+
+#' Pick default color
+#' @param n An integer
+#' @importFrom grDevices hcl
+#' @export
+gg_color_hue=function(n){
+  hues=seq(15,375,length=n+1)
+  hcl(h=hues,l=65,c=100)[1:n]
+}
 
 #'Add lines with labels to pre-existing ggplot
 #'@param p An object of class ggplot
@@ -43,6 +52,7 @@ calEquation=function(fit,mode=1,pred=NULL,modx=NULL,modx.values=NULL,label=NULL,
 #'@param lty line type
 #'@param color line color
 #'@param size line size
+#'@param add_theme_bw2 logical Whether or not add theme_bw2()
 #'@param ... Further arguments to be passed to geom_text
 #'@importFrom ggplot2 ggplot stat_function
 #'@export
@@ -52,18 +62,57 @@ calEquation=function(fit,mode=1,pred=NULL,modx=NULL,modx.values=NULL,label=NULL,
 #'df=calEquation(fit)
 #'p=ggplot(data=mtcars,aes(x=wt,y=mpg))
 #'add_lines(p,df)
-#'add_lines(p,df,lty=1:3,color=1:3,size=1)+theme_bw()
+#'add_lines(p,df,lty=1:3,color=1:3,size=1)
 #'fit=lm(mpg~wt*vs,data=mtcars)
 #'df=calEquation(fit)
 #'p=ggplot(data=mtcars)+geom_point(aes(x=wt,y=mpg))
 #'add_lines(p,df)
 #'add_lines(p,df,lty=1:2,color=1:2,size=1)+theme_bw()
-add_lines=function(p,df,xpos=0.3,add.coord.fixed=TRUE,lty=1,color="black",size=0.5,...){
-
+add_lines=function(p,df,xpos=0.3,add.coord.fixed=TRUE,lty=NULL,color=NULL,size=0.5,add_theme_bw2=TRUE,...){
+      # xpos=0.3;add.coord.fixed=TRUE;lty=1;color="black";size=0.50
      count=nrow(df)
+     if(is.null(df$lty)) {
+         if(is.null(lty)) {
+             if(count<=6) {
+                df$lty=1:count
+             } else{
+                df$lty=(1:count)%%6
+             }
+         } else {
+             df$lty=lty
+         }
+     }
+     if(is.null(df$color)) {
+        if(is.null(color)) {
+            df$color=gg_color_hue(count)
+        } else{
+           df$color=color
+        }
+     }
+     if(is.null(df$size)) df$size=size
+     df
+     fun=list()
+     statfun=list()
+     for(i in 1:count){
+          fun[[i]]=local({
+               j<-i
+               function(x){
+                    df$slope[j]*x+df$intercept[j]
+               }
+          })
+          statfun[[i]]=local({
+               j<-i
+               stat_function(fun=fun[[j]],lty=df$lty[j],color=df$color[j],size=size)
+          })
+
+     }
+     for(i in 1:count){
+          p<-p+statfun[[i]]
+     }
+     p
      info=getAspectRatio(p)
      ratio=info$ratio
-          df$slope2=df$slope*ratio
+     df$slope2=df$slope*ratio
      df$radian=atan(df$slope2)
      df$angle=df$radian*180/pi
      if(!is.null(df$xpos)) xpos=df$xpos
@@ -72,33 +121,21 @@ add_lines=function(p,df,xpos=0.3,add.coord.fixed=TRUE,lty=1,color="black",size=0
      df$x=x
      df$y=df$x*df$slope+df$intercept
      if(is.null(df$vjust)) df$vjust=c(rep(-0.5,count-1),1.5)
-     if(is.null(df$lty)) df$lty=lty
-     if(is.null(df$color)) df$color=color
-     if(is.null(df$size)) df$size=size
-     df
-     fun=list()
-     statfun=list()
-     for(i in 1:count){
-        fun[[i]]=local({
-             j<-i
-             function(x){
-               df$slope[j]*x+df$intercept[j]
-             }
-             })
-        statfun[[i]]=local({
-             j<-i
-             stat_function(fun=fun[[j]],lty=df$lty[j],color=df$color[j],size=size)
-        })
 
-     }
-     for(i in 1:count){
-       p<-p+statfun[[i]]
-     }
+     df
+
      p<-p+geom_text(data=df,aes_string(x="x",y="y",label="label",angle="angle",vjust="vjust"),color=df$color,...)
      if(add.coord.fixed) p<-p + coord_fixed(ratio=ratio)
+     if(add_theme_bw2) p <- p+theme_bw2()
      p
 }
 
 
-
-
+#' theme_bw with no grid
+#' @importFrom ggplot2 theme_bw theme element_blank
+#' @export
+theme_bw2=function(){
+   theme_bw()+
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
+}
